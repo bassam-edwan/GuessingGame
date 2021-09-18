270a804c25a66aea6ac0ed819f329728bfc376d2
bassam-edwan <88935668+bassam-edwan@users.noreply.github.com>
2021-09-18 16:18:10 +0300

    Update scores.js

--- js/scores.js
+++ js/scores.js
@@ -1,3 +1,12 @@
 getScores().then((data)=>{
-    console.log(data)
+    //console.log(data)
+    let display = ''
+    for(let i=0 ;i<data.length; i++){
+       
+        display += `<li class="score-element list-header">
+            <span>${data[i].name} </span>
+            <span>${data[i].score} </span>
+        </li>`
+    }
+    document.getElementById("displayScores").innerHTML = display
 })
